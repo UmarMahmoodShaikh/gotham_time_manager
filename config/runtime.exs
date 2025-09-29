1f6fcf1109957daae703ce9ba43570ea1665c760
UmarMahmoodShaikh <umarmahmoodshk@gmail.com>
2025-09-29 17:35:19 +0200

    no message

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,6 +33,7 @@ if config_env() == :prod do
   config :gotham_time_manager, GothamTimeManager.Repo,
     url: database_url,
     ssl: true,
+    ssl_opts: [verify: :verify_none],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
