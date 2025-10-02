0530ac75d947dbefd2a2f65bf9ea73fed7d09cdd
UmarMahmoodShaikh <umarmahmoodshk@gmail.com>
2025-10-02 17:42:25 +0200

    no message

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,8 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :gotham_time_manager, GothamTimeManager.Repo,
-    ssl: true,
-    ssl_opts: [
+    ssl: [
       verify: :verify_none
     ],
     url: database_url,
